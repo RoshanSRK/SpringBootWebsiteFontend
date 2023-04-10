80ef3f7ea66a88dab520b72322272cd62fe9c916
RoshanSRK <101582132+RoshanSRK@users.noreply.github.com>
2023-04-10 22:55:58 +0400

    Added method to package and send data to API as a json

--- springboot_website/login/js/forms.js
+++ springboot_website/login/js/forms.js
@@ -5,7 +5,24 @@ const registerBtn = document.querySelector('#register-btn');
 const registerUser =  async() => {
 
     let response = await fetch('http://localhost:8080/api/v1/user/all');
+
+    let response2 = await fetch('http://localhost:8080/api/v1/user/save',
+    {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body:{
+            "name": "roshan",
+            "email": "roshan@gmail.com",
+            "password": "123456789",
+            "dob": "2003-04-10"
+          }
+
+    })
     console.log(response);
+    console.log(response2);
 
 }
 
